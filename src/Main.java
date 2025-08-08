40d54d58d6ce80d43f4a7e022d36c62263c33373
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-08-08 23:10:24 +0530

    Services added

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ProductService service = new ProductService();
+
+//        Product p = new Product("Asus Tuf", "Laptop", "Brown Table", 2023);
+//
+//        service.addProduct(p);
+
+        service.addProduct(new Product("Asus Tuf", "Laptop", "Brown Table", 2023));
+        service.addProduct(new Product("Asus Notebook", "Laptop", "Brown Table", 2015));
+        service.addProduct(new Product("Redmi 12 5G", "Phone", "Black Table", 2023));
+        service.addProduct(new Product("Redmi Y1", "Phone", "Drawer", 2015));
+        service.addProduct(new Product("Charger 33W", "Charger", "Drawer", 2030));
+        service.addProduct(new Product("iball 16GB Pendant", "Pendrive", "Cupboard Drawer", 2023));
+        service.addProduct(new Product("Sandisk 32GB", "Pendrive", "Cupboard Drawer", 2023));
+
+//        List<Product> products = service.getAllProducts();
+//        for(Product p : products){
+//            System.out.println(p);
+//        }
+        System.out.println("=====================================================================================================");
+
+        System.out.println("A particular product");
+
+        Product p = service.getProduct("Redmi Y1");
+        System.out.println(p);
+
+        System.out.println("=====================================================================================================");
+
+        System.out.println("Get product by its place");
+        List<Product> p1 = service.getByPlace("Drawer");
+        for(Product p2 : p1){
+            System.out.println(p2);
+        }
+
+        System.out.println("=====================================================================================================");
+        System.out.println("A particular text");
+
+        List<Product> prods = service.getProductWithText("brown");
+        for(Product product : prods){
+            System.out.println(product);
+        }
+
+    }
+}
---
+++ src/ProductService.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService {
+
+    List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p){
+        products.add(p);
+    }
+
+    public List<Product> getAllProducts(){
+        return products;
+    }
+
+    public Product getProduct(String name){
+        for(Product p : products){
+            if(p.getName().equals(name))
+                return p;
+        }
+
+        return null;
+    }
+
+    public List<Product> getByPlace(String name){
+        List<Product> result = new ArrayList<>();
+        for(Product p : products){
+            if(p.getPlace().equalsIgnoreCase(name))
+                result.add(p);
+        }
+        return result;
+    }
+
+    public List<Product> getProductWithText(String str){
+        String text = str.toLowerCase();
+        List<Product> prods = new ArrayList<>();
+
+        for(Product p : products){
+            String name = p.getName().toLowerCase();
+            String type = p.getType().toLowerCase();
+            String place = p.getPlace().toLowerCase();
+            if(name.contains(text) || type.contains(text) || place.contains(text))
+                prods.add(p);
+        }
+
+        return prods;
+    }
+}
